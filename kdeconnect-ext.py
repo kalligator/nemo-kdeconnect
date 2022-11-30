9dea5da63563564e3652ffe2f54f1e357b694789
Navid Zamani <navid@Fuchs1>
2022-11-30 20:11:13 +0100

    Fixed & cleaned up
    • Fixed device detection.
    • Fixed zipping.
    • Added Warnings about zipping bugs.
    • General cleanup of console output.

--- kdeconnect-ext.py
+++ kdeconnect-ext.py
@@ -5,6 +5,8 @@ from kdeconnect import send_files, get_available_devices
 import zipfile
 import urllib
 import urllib.parse as urlparse
+from urllib.request import url2pathname
+from os import remove
 
 TARGET = "%%TARGET%%".title()
 Nautilus = importlib.import_module("gi.repository.{}".format(TARGET))
@@ -19,15 +21,15 @@ class KDEConnectExtension(GObject.GObject, Nautilus.MenuProvider):
         import os
         for root, dirs, files in os.walk(path):
             for file in files:
-                print(dirs + [file])
+                #print("KDEConnectExtension: zipdir: dirs+[file]: "+str(dirs+[file]))
                 ziph.write(os.path.join(root, file), os.path.join(*(dirs + [file])))
 
     def menu_activate_cb(self, menu, files, device_id, device_name, folder_list):
         import zipfile
         import os
         for folder_name in folder_list:
-            zip_name = urllib.url2pathname(urlparse.urlparse(folder_name + '.zip').path)
-            zipf = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
+            zip_name = url2pathname(urlparse.urlparse(folder_name + '.zip').path)
+            zipf = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) # TODO: BUG/WARNING: Overwrites already existing files with the same name!!
             self.zipdir(zip_name[:-4] + '/', zipf)
             zipf.close()
             zipfile = Nautilus.FileInfo.create_for_uri(folder_name + '.zip')
@@ -35,11 +37,15 @@ class KDEConnectExtension(GObject.GObject, Nautilus.MenuProvider):
 
         send_files(files, device_id, device_name)
 
+        # TODO: BUG: Leaves ZIP files behind. Can’t delete them, as kdeconnect hasn’t opened them yet.
+        #            Maybe watch for the file to be opened, then run send_file(), then when the file user goes away, delete it.
+        #            But asynchronously, to not block this thread.
+
     def get_file_items(self, window, files):
         try:
             devices = get_available_devices()
         except Exception as e:
-            raise Exception("Failed to get available devices")
+            raise Exception(" KDEConnectExtension: get_file_items: Failed to get available devices")
 
         files_new = []
         folder_list = []
@@ -50,8 +56,8 @@ class KDEConnectExtension(GObject.GObject, Nautilus.MenuProvider):
                 folder_list.append(files[i].get_uri())
 
         files = files_new
-        print(files)
-        print(folder_list)
+        #print("KDEConnectExtension: get_file_items: files: "+str(files))
+        #print("KDEConnectExtension: get_file_items: folder_list: "+str(folder_list))
 
         if (len(files_new) + len(folder_list) < 1):
             return []
--- kdeconnect.py
+++ kdeconnect.py
@@ -1,18 +1,25 @@
-from subprocess import check_output, call
-import re
+from subprocess import run, check_output
+from re import search, sub
+from urllib.parse import unquote
+from urllib.request import url2pathname
 
 def send_files(files, device_id, device_name):
+    #print("KDEConnectExtension: Sharing Files:")
     for file in files:
-        return_code=call(["kdeconnect-cli", "-d", device_id, "--share", file.get_uri()])
+        #print("  "+sub("^file://","",url2pathname(file.get_uri()))) # +": "), end='')
+        result = run(["kdeconnect-cli", "-d", device_id, "--share", file.get_uri()],capture_output=True)
+        #if result.returncode == 0: # TODO: KDEConnect-cli doesn’t seem to block, and so can’t return an error code on failure. Stupid pseudo-cli!
+        #    print("Sucess")
+        #else:
+        #    print("Failed!\n    "+("\n    ".join(result.stderr.decode("utf-8").strip().split("\n"))))
 
 def get_available_devices():
-    devices_a=[]
-    devices = check_output(["kdeconnect-cli", "-a"]).decode("utf-8").strip().split("\n")
-    devices.pop()
-    print(devices)
+    devices_a = []
+    devices = run(["kdeconnect-cli", "-a"],capture_output=True).stdout.decode("utf-8").strip().split("\n")
+    #print("KDEConnectExtension: Found "+str(len(devices))+" device(s)")
     for device in devices:
-        device_name=re.search("(?<=-\s).+(?=:\s)", device).group(0)
-        device_id=re.search("(?<=:\s)[a-z0-9_]+(?=\s\()", device).group(0).strip()
+        device_name = search("(?<=-\s).+(?=:\s)"         , device).group(0)
+        device_id   = search("(?<=:\s)[a-z0-9_]+(?=\s\()", device).group(0).strip()
         devices_a.append({ "name": device_name, "id": device_id })
-    print(devices_a)
+        #print("  " +device_name+" ("+device_id+")")
     return devices_a
